28257efcdc988d497887b53e83d6ba85df5a0cf3
batmunkh namshir <namshir17@gmail.com>
2022-01-11 22:47:36 -0800

    commit tree 345d82cebf1f68595d4b2f2c5e6937b68dd606e2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase()
+}
+
+
+
+function setBestCustomer () {
+    bestCustomer = "not bob"; // no variable assigned turns it into global
+}
+const changeLeastFavoriteCustomer = "?";
+
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const changeLeastFavoriteCustomer = "init value";
+
+function changeLeastFavoriteCustomer() {
+   changeLeastFavoriteCustomer = "After value";
+}
